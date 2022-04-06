fe2694570be186e4242dd16f8a2e078411892926
Eric Johnson <ericdavidjohns@gmail.com>
2022-04-06 17:13:58 -0600

    stubbed in HexGame methods

--- src/HexGame.java
+++ src/HexGame.java
@@ -1,2 +1,34 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 public class HexGame {
+    private int size;
+    private DisjointSet gameboard;
+
+    public HexGame(int dimensions) {
+        this.size = dimensions;
+        gameboard = new DisjointSet(size*2);
+    }
+
+    public boolean playBlue(int position, boolean displayNeighbors) {
+
+    }
+    public boolean playRed(int position, boolean displayNeighbors) {
+
+    }
+    public Color[] getGrid() {
+
+    }
+    public int getSize() {
+        return size;
+    }
+    private boolean isOccupied(int position) {
+
+    }
+    private ArrayList<Integer> getNeighborsRed(int position) {
+
+    }
+    private ArrayList<Integer> getNeighborsBlue(int position) {
+
+    }
 }
