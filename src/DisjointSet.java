57b6415963547bd292c965836a87ef6d778cb5b0
Eric Johnson <ericdavidjohns@gmail.com>
2022-04-05 23:10:10 -0600

    finished the disjoint set class

--- src/DisjointSet.java
+++ src/DisjointSet.java
@@ -1,16 +1,34 @@
+import java.util.Arrays;
+
 public class DisjointSet {
     private int[] size;
 
     public DisjointSet(int size) {
         this.size = new int[size];
+        Arrays.fill(this.size, -1);
     }
 
 
-    public void union(int root1, int root2) {
-
+    public void union(int root1, int root2) { // smart union by size
+        // negative number means size, positive number is the parent of the node
+        // root1 and root2 are not in the same tree - must do a find first
+        if (find(root1) != find(root2)) {
+            if (size[root2] < size[root1]) {
+                size[root2] += size[root1];
+                size[root1] = root2;
+            } else {
+                size[root1] += size[root2];
+                size[root2] = root1;
+            }
+        }
     }
 
     public int find(int node) {
-        return -1;
+        if (size[node] < 0) { // if it is a negative value, it is the root, and its value should be returned
+            return node;
+        } else {
+            int findParent = find(size[node]);
+            return size[node] = findParent;
+        }
     }
 }
